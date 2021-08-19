51725c7268933d6288d77ef6e275039fc177017b
carldavid309 <89143358+carldavid309@users.noreply.github.com>
2021-08-19 12:05:27 +0800

    Add files via upload

---
+++ UML-SettersAndGetters/src/com/company/Admin.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Admin {
+
+    private String nameAdmin;
+    private String department;
+    private int id;
+
+    public String getNameAdmin(){
+        return nameAdmin;
+    }
+    public void setNameAdmin(String nameAdmin){
+        this.nameAdmin = nameAdmin;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public void setDepartment(String department){
+        this.department = department;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ UML-SettersAndGetters/src/com/company/Teacher.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Teacher {
+
+    private String nameTeacher;
+    private String department;
+    private int id;
+    private int phone;
+
+    public String getNameTeacher(){
+        return nameTeacher;
+    }
+    public void setNameStudent(String nameTeacher){
+        this.nameTeacher = nameTeacher;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public void setDepartment(String department){
+        this.department = department;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getPhone(){
+        return phone;
+    }
+    public void setPhone(int phone){
+        this.phone = phone;
+    }
+}
